ba13f165168fec6dca9d0a168d751c39fd0702ee
BRama10 <grand.hunter.dark.15@gmail.com>
2024-07-01 19:06:53 -0400

    f

--- backend/sample_output/interact_downloaded.py
+++ backend/sample_output/interact_downloaded.py
@@ -5,19 +5,16 @@ import sys
 import json
 import pprint
 import requests
-
-
 class Interact:
-    def __init__(self, file_path: str | None = None, name: str = '', config: str = 'config.json', output: str | None = None):
+    def __init__(self, file_path: str | None = None, name: str = '', config: str = 'config.json'):
         self.file_path = file_path
         self.minified_code: str | None = None
         self.encoded_string: str | None = None
-        self.output_file_path: str | None = output
+        self.output_file_path: str | None = None
         self.reqs: str | None = None
         self.compressed_reqs: str | None = None
         self.name = name
         self.config = config
-
     def minify_python_code(self):
         with open(self.file_path, 'r') as file:
             lines: list[str] = file.readlines()
@@ -28,13 +25,11 @@ class Interact:
                 minified_lines.append(stripped_line)
         self.minified_code = "\n".join(minified_lines)
         return self
-
     def compress(self):
         compressed_data = gzip.compress(self.minified_code.encode('utf-8'))
         encoded_data = base64.b64encode(compressed_data)
         self.encoded_data = encoded_data.decode('utf-8')
         return self
-
     def compress_reqs(self):
         with open(f"meta_{self.name}_reqs.txt", 'r') as file:
             self.reqs: str = file.read()
@@ -44,44 +39,32 @@ class Interact:
         compressed = gzip.compress(minified.encode('utf-8'))
         self.compressed_reqs = base64.b64encode(compressed).decode('utf-8')
         return self
-
     def decompress_reqs(self):
         decompressed = gzip.decompress(base64.b64decode(self.compressed_reqs))
         minified = decompressed.decode('utf-8')
         expanded = minified.replace(';', '\n')
-
         output_filename = f"meta_{self.name}_reqs.txt"
-
-        with open(''.join(self.output_file_path.split('/')[:-1])+'/'+output_filename, 'w') as file:
+        with open(output_filename, 'w') as file:
             file.write(expanded)
-
         return self
-
     def decompress(self):
         compressed_data = base64.b64decode(self.encoded_string)
         decompressed_data = gzip.decompress(compressed_data)
-
         with open(self.output_file_path, 'w', newline='') as file:
             file.write(decompressed_data.decode('utf-8'))
-
         return self
-
     def get_compressed_data(self):
         return {
             'python': self.encoded_data,
             'reqs': self.compressed_reqs
         }
-
     def upload(self):
         with open(self.config, 'r') as file:
             data: dict[str, dict] = json.load(file)
-
         meta = data.get('meta')
-
         headers = {
             "Content-Type": "application/json",
         }
-
         upload_content = {
             'name': meta.get('name'),
             'author': meta.get('author'),
@@ -91,40 +74,29 @@ class Interact:
             'file_data': self.encoded_data,
             'dependencies': self.compressed_reqs
         }
-
         url = 'https://openagi-beta.vercel.app/api/upload'
-
         pprint.pprint(upload_content)
-
         response = requests.post(url,
                                  data=json.dumps(upload_content),
                                  headers=headers,
                                  )
-        
         pprint.pprint(response.content)
-
         return self
-
-    def download_agent(self) :
-        query = f'https://openagi-beta.vercel.app/api/download?name={self.name}'
+    def download_agent(name: str, version: float | None) :
+        if version:
+            query = f'https://localhost:3000/api/download?version={version}&name={name}'
+        else:
+            query = f'http://localhost:3000/api/download?name={name}'
         response = requests.get(query)
-        response: dict = response.json()
-
-        self.encoded_string = response.get('file_data')
-        self.compressed_reqs = response.get('dependencies')
-
-        return self
-
-    def install(self):
-        with open(f"{''.join(self.output_file_path.split('/')[:-1])}/meta_{self.name}_reqs.txt", 'r') as file:
+        print(response.json())
+    def _install(self):
+        with open(f"meta_{self.name}_reqs.txt", 'r') as file:
             requirements = file.readlines()
         for requirement in requirements:
             subprocess.check_call(
                 [sys.executable, "-m", "pip", "install", requirement.strip()])
         subprocess.check_call([sys.executable, "-m", "playwright", "install"])
         return self
-    
 if __name__ == '__main__':
-    client = Interact('web_agent.py', name='web', output='sample_output/interact_downloaded.py')
-    # client.download_agent().decompress().decompress_reqs().install()
-    # client.minify_python_code().compress().compress_reqs().upload()
+    client = Interact('interact.py', name='web')
+    client.minify_python_code().compress().compress_reqs().upload()
